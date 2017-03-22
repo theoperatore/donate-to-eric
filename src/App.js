6d29984e48979914d76b09c1828b27d9aac50b20
Al Petersen <al@spotify.com>
2017-03-22 10:44:30 -0400

    pages

--- src/App.js
+++ src/App.js
@@ -4,17 +4,10 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return (<main>
+      <h1>He needs your help!</h1>
+      <a href="https://www.amazon.com/Mass-Effect-Andromeda-PlayStation-4/dp/B01N00X3DH?th=1" className='donate'>Donate to Eric</a>
+    </main>);
   }
 }
 
